fba2df0f800f6441b04f72b29d71feeedb29a199
Yegor Gangalo <gangaloyegor@gmail.com>
2021-01-03 01:33:04 +0200

    add style css & propTypes

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,8 @@ class App extends Component {
         {id: 'id-2', name: 'Hermione Kline', number: '443-89-12', experience: 'middle'},
         {id: 'id-3', name: 'Eden Clements', number: '645-17-79', experience: 'junior'},
         {id: 'id-4', name: 'Annie Copeland', number: '227-91-26', experience: 'senior'},
-        {id: 'id-5', name: 'Ann', number: '227-91-46', experience: 'junior'},
+        {id: 'id-5', name: 'Ann Hits', number: '227-91-46', experience: 'junior'},
+        {id: 'id-6', name: 'Ed Clirence', number: '217-01-46', experience: 'middle'},
       ],
     }
 
@@ -50,9 +51,9 @@ class App extends Component {
       const { contacts, filter } = this.state;
 
       return (<>
-        <h1>Phonebook</h1>
+        <h1 className="title">Phonebook</h1>
         <ContactForm onSubmit={this.formSubmitHandler} contacts={contacts} />
-        <h2>Contacts</h2>
+        <h2 className="title">Contacts</h2>
         <Filter value={filter} onChange={this.changeFilter} />
         <ContactList contacts={contacts} filter={filter} deleteContact={this.deleteContact} />
         </>
--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import s from './ContactList.module.css';
 
 class ContactList extends Component {
     static propTypes = {
-        //
-    }
-
-    state = {
-
+      deleteContact: PropTypes.func.isRequired,
+      filter: PropTypes.string.isRequired,
+        contacts: PropTypes.arrayOf(
+          PropTypes.shape({
+            id: PropTypes.string.isRequired,
+            name: PropTypes.string.isRequired,
+            number: PropTypes.string.isRequired,
+            experience: PropTypes.string.isRequired,
+        })),
     }
 
     filterContacts = () => {
@@ -19,12 +25,12 @@ class ContactList extends Component {
     render() {
         const filteredContacts = this.filterContacts();
 
-        return <ul>
+        return <ul className={s.contactList} >
           {filteredContacts.map(({ id, name, number, experience }) =>
-            <li key={id}>
-              <span>{name}: </span>
-              <span>{number}, </span>
-              <span>{experience};</span>
+            <li key={id} className={s.item} >
+              <span>{name}:</span>
+              <span>{number},</span>
+              <span>{experience}</span>
               <button type="button" onClick={()=> this.props.deleteContact(id)} >Delete</button>
             </li>)}
         </ul>
